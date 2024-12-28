fa8b971e4e81acf9fc5dbc1438ac0ff0834b4ad6
Ismail Hossain <52321084+ismail306@users.noreply.github.com>
2024-12-28 12:44:17 +0600

    Update 2024_12_27_042402_create_tasks_table.php

--- database/migrations/2024_12_27_042402_create_tasks_table.php
+++ database/migrations/2024_12_27_042402_create_tasks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('title');
             $table->text('description')->nullable();
             $table->enum('status', ['Pending', 'In Progress', 'Completed'])->default('Pending');
